c124f1465c30b12458133fa589e13825ee83d95a
Alan Casas <alancasasarevalo@gmail.com>
2018-02-13 18:16:57 +0100

    Domain module added Multidex implementation added MadridGoToShoppingApp added (AppDelegate for iOS)

---
+++ app/src/main/java/alancasasarevalo/com/gotoshopping/MadridGoToShoppingApp.kt
@@ -0,0 +1,17 @@
+package alancasasarevalo.com.gotoshopping
+
+import android.support.multidex.MultiDexApplication
+
+class MadridGoToShoppingApp: MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+
+    }
+
+}
